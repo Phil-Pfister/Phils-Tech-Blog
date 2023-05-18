b443f98e4ed6d0d15b677a3d4ee1f22578aeb57f
Phil-Pfister <salshouse@gmail.com>
2023-05-18 09:02:59 -0500

    added file structure elements

---
+++ controllers/home-routes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const Post = require('../models/Posts');
+
+// route to get all dishes
+router.get('/', async (req, res) => {
+    const postData = await Post.findAll().catch((err) => { 
+        res.json(err);
+      });
+        const dishes = postData.map((post) => post.get({ plain: true }));
+        res.render('all', { posts });
+      });
+  
+  // route to get one dish
+  router.get('/post/:id', async (req, res) => {
+    try{ 
+        const postData = await Post.findByPk(req.params.id);
+        if(!postData) {
+            res.status(404).json({message: 'No post with this id!'});
+            return;
+        }
+        const dish = postData.get({ plain: true });
+        res.render('post', post);
+      } catch (err) {
+          res.status(500).json(err);
+      };     
+  });
+
+module.exports = router;
